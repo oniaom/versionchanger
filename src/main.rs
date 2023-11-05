073f31f1dc884be1a1900bee42e6141a9f40045d
oniaom <github@sl.tasosn.com>
2023-11-05 16:18:39 +0200

    commit message

---
+++ src/main.rs
@@ -0,0 +1,128 @@
+use regex::Regex;
+use std::env;
+use std::fs;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::PathBuf;
+
+
+fn handle_args(args: Vec<String>) -> Option<String> {
+    if args.len() < 2 {
+        return None;
+    }
+    return Some(args[1].to_string());
+}
+
+fn replace_version(file: String) -> Result<(), std::io::Error> {
+    let file1: File = File::open(file.clone())?;
+    let reader = io::BufReader::new(file1);
+
+    // Define a regular expression to match "version=" and a number
+    let re: Regex = Regex::new(r"version=(\d+)").unwrap();
+    let mut new_file_content: String = String::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        let new_line = re.replace_all(&line, |caps: &regex::Captures| {
+            let num = caps.get(1).unwrap().as_str().parse::<u32>().unwrap() + 1;
+            format!("version={}", num)
+        });
+
+        new_file_content.push_str(&new_line);
+        new_file_content.push('\n');
+    }
+
+    fs::write(file, new_file_content)?;
+
+    Ok(())
+}
+
+fn get_folder_path(file: String) -> Option<String> {
+    // Return the path of the file, without the file name
+    let stuff: Vec<&str> = file.split('\\').collect();
+
+    // get len
+    let length = stuff.len();
+    // Before anything, if len = 1 then end!
+    if length <= 1 {
+        return None;
+    }
+
+    let mut result = PathBuf::new();
+
+    for i in 0..length - 1 {
+        // For some reason it doesn't add the first backslash...
+        if i == 0 {
+            result.push(stuff[i].to_owned() + "\\");
+            continue;
+        }
+        result.push(stuff[i]);
+    }
+
+    let a = result.as_path().to_str().map(String::from);
+
+    a
+}
+
+fn get_file_name(file: String) -> Option<String> {
+    let stuff: Vec<&str> = file.split('\\').collect();
+    if let Some(last_string) = stuff.last() {
+        println!("The last string is: {}", last_string);
+        // Return the string
+        Some(String::from(last_string.to_string()))
+    } else {
+        None
+    }
+}
+
+fn backup_old_file(file: String) -> Option<bool> {
+    let folder_path_option = get_folder_path(file.clone());
+    if folder_path_option.is_none() {
+        println!("Error parsing folder path.");
+        return None;
+    }
+    
+    let folder_path: String = folder_path_option.unwrap();
+    let file_name_option = get_file_name(file.clone());
+
+    let mut _file_name: String = String::new();
+    match file_name_option {
+        Some(name) => _file_name = name,
+        None => return None,
+    }
+
+    let mut final_path = PathBuf::from(folder_path.clone());
+    let backup_file_name: String = _file_name + ".old";
+    final_path.push(backup_file_name);
+    let x = String::from(final_path.to_str().unwrap());
+
+    fs::copy(file, x.clone()).ok();
+    println!("{folder_path}");
+    println!("{x}");
+    return Some(true);
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let target: Option<String> = handle_args(args);
+    if target.is_none() {
+        println!("No file selected");
+        return;
+    }
+    let file: String = target.unwrap();
+
+    // Backup old file first!
+    let backup_ok = backup_old_file(file.clone());
+    if backup_ok.is_none() {
+        println!("Something bad in backup_old_file");
+        return;
+    }
+
+    
+    let res: Result<(), io::Error> = replace_version(file);
+    match res {
+        Ok(_) => {}
+        Err(err) => println!("{err}"),
+    }
+    
+}
